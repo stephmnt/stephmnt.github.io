49b667825558c1abf38baa49299577da82980260
stephmnt <contact@stephanemanet.com>
2019-01-02 21:38:09 +0100

    mise en style du script medium

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -13,17 +13,17 @@ $(function () {
 				 } else {
 					 visibleSm = ' visible-sm';
 				 }
-				output += '<div class="medium-post ' + visibleSm + '"><a href="'+ item.link + '">';
+				output += '<div class="medium-post ' + visibleSm + '"><a class="post-box" href="'+ item.link + '">';
 				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
 				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
 				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
 				output += '<div class="post-image"><img class="img-responsive" src="' + src + '" width="360px" height="240px" /></div>';
-				output += '<div class="post-content"><h3>' + item.title + '</h3>';
+				output += '<div class="post-caption"><div class="post-content"><h3 class="post-h3">' + item.title + '</h3>';
 				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-		    output += '</div>';
-				output += '</div><div style="clear:both"></div>';
+		    output += '</div></div>';
+				output += '</div>';
 				return k < 3;
 			});
 
