394e3b386801a6ddd38fc0480f0c0574c27a6244
stephmnt <contact@stephanemanet.com>
2019-01-13 17:29:42 +0100

    maj design medium

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -13,17 +13,24 @@ $(function () {
 				 } else {
 					 visibleSm = ' visible-sm';
 				 }
+				output += '<div class="medium-post">';
 				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
 				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
 				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
-				output += '<div class="medium-post ' + visibleSm + '" style="background-image:url(' + src + ');"><a class="post-box" href="'+ item.link + '">';
-				output += '<div class="post-image"><img class="img-responsive" src="' + src + '" /></div>';
-				output += '<div class="post-caption"><div class="post-content"><h3 class="post-h3">' + item.title + '</h3>';
 				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-		    output += '</div></div>';
-				output += '</div>';
+				var maxLength = 1200 // maximum number of characters to extract
+				//trim the string to the maximum length
+				var trimmedString = yourString.substr(0, maxLength);
+				//re-trim if we are in the middle of a word
+				trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+				output += '<h2 href="'+ item.link +'">' + item.title + '</h2>';
+				output += '<img class="img-responsive" src="' + src + '" alt="' + item.title + '" />';
+				output += '<div class="post-details"><span class="post-date">' + item.pubDate + "</span>";
+				output += '<span class="social-share"><a href="https://www.facebook.com/sharer/sharer.php?u='+ item.link +'" title="Partager sur Facebook" target="_blank" class="tag"><span class="term"><i class="fab fa-facebook-square"></i> Partager</span></a><a href="https://twitter.com/intent/tweet?text=' + item.title + ' par @stephanemanet '+ item.link +'" title="Partager sur Twitter" target="_blank" class="tag"><span class="term"><i class="fab fa-twitter-square"></i> Tweeter</span> </a><a href="https://www.linkedin.com/sharing/share-offsite/?url='+ item.link +'" title="Partager sur LinkedIn+" target="_blank" class="tag"><span class="term"><i class="fab fa-linkedin"></i> Partager</span> </a></span></div>';
+				output += '<p>' + trimmedString + '...</p>';
+				output += '<a href="'+ item.link +'" class="btn">Lire la suite</a></div>';
 				return k < 3;
 			});
 
